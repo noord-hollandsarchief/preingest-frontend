a5d020065d9bcd60f0d2003bd6f6b224f14cc8bf
Wai Wong <73949448+WhyYellow@users.noreply.github.com>
2022-03-16 11:44:10 +0100

    change: make it optional

--- src/services/PreingestApiService.ts
+++ src/services/PreingestApiService.ts
@@ -193,7 +193,7 @@ export const stepDefinitions: Step[] = [
   {
     id: 'indexing',
     dependsOn: ['unpack'],
-    requiredSettings: ['schemaToValidate'],
+    //requiredSettings: ['schemaToValidate'],
     actionName: 'IndexMetadataHandler',
     description: 'MS Excel - Metadatabestanden indexeren',
     allowRestart: true,
@@ -414,7 +414,7 @@ export const stepDefinitions: Step[] = [
   {
     id: 'indexing',
     dependsOn: ['unpack'],
-    requiredSettings: ['schemaToValidate'],
+    //requiredSettings: ['schemaToValidate'],
     actionName: 'IndexMetadataHandler',
     description: 'MS Excel - Metadatabestanden indexeren',
     allowRestart: true,
